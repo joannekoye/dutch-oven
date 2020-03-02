270190369cd9f66924b1b05d760d775493a0231c
Joan Nekoye <joannekoye@gmail.com>
2020-03-02 09:30:59 +0300

    edit js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-//Beginning of Business logic
 
-
-//Beginning of UI
 $(document).ready(function(event) {
   $('#pickUp').click(function() {
     $("#pickUpOption").show();
@@ -13,26 +10,6 @@ $(document).ready(function(event) {
     $('#displayNumber').show();
     $("#pickUpOption").hide();
   })
-  $('#placeOrder').click(function() {
-    var location = $('input#location').val();
-    var phoneNumber = $('input#number').val();
-    var newContact = new contact (phoneNumber, location)
-    $('#deliverOption').show();
-    $('#contactAddress').text( + newContact.deliveryAddress);
-
-    var type = $('select#pizzaType').val();
-    var size = $('select#pizzaSize').val();
-    var crust = $('select#crust').val();
-
-    var toppings = [];
-    $.each($("input[name='toppings']:checked"), function(){
-      toppings.push($(this).val());
-    });
-    var allTopings = toppings.join(", ");
-    var pizzaDetails=(size + " " + type+", " +crust+" crust. Toppings: " +allTopings);
-    var newOrder = new order (pizzaSize,crust);
-    newOrder.pizzaCost();
-    totalPriceArray.push(newOrder.pizzaPrice);
-    $('#orderDetails').append(+ pizzaDetails);
-  })
+  
+    
 })
