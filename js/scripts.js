17ac98f534af8ba2fadf7114d2c0026c0ae9644f
Joan Nekoye <joannekoye@gmail.com>
2020-03-02 08:33:39 +0300

    edit jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,7 @@
-var totalPriceArray = [];
-function order (pizzaSize, crust) {
-  this.type = 5;
-  this.size= pizzaSize;
-  this.crust = crust;
-  this.toppings = 3;
-  this.pizzaPrice = 0;
-}
-order.prototype.pizzaCost = function () {
-  if(this.pizzaSize === "Small(serves 1)") {
-    this.pizzaPrice += 3;
-  }else if(this.pizzaSize ==="Medium(serves 2)") {
-    this.pizzaPrice +=6;
-  }else if(this.pizzaSize==="Large(serves 3-4)") {
-    this.pizzaPrice +=9;
-  }else if(this.pizzaSize==="Mega Large(serves 6)") {
-    this.pizzaPrice +=9;
-  }
-
-  if (this.crust === "") {
-    this.pizzaPrice += 0;
-  }else if(this.crust==="") {
-    this.pizzaPrice +=1;
-  }else if(this.crust==="") {
-    this.pizzaPrice +=2;
-  }
-
-  this.pizzaPrice += this.toppings;
-  return this.pizzaPrice;
-}
-order.prototype.finalCost = function () {
-  var totalCartPrice = 0;
-  for (var arrayElement = 0; arrayElement<totalPriceArray.length;arrayElement++){
-    totalCartPrice += totalPriceArray[arrayElement];
-  }
-  return totalCartPrice;
-}
-
-function Address (phoneNumber, location) {
-  this.phoneNumber = phoneNumber;
-  this.location = location;
-  this.deliveryAddress = (phoneNumber + "  " + location);
-}
+//Beginning of Business logic
 
 
-
-
-
-
-
-
-
-
-
-
-
+//Beginning of UI
 $(document).ready(function(event) {
   $('#pickUp').click(function() {
     $("#pickUpOption").show();
@@ -65,8 +13,7 @@ $(document).ready(function(event) {
     $('#displayNumber').show();
     $("#pickUpOption").hide();
   })
-  $('form').submit(function(event) {
-    event.preventDefault();
+  $('#placeOrder').click(function() {
     var location = $('input#location').val();
     var phoneNumber = $('input#number').val();
     var newContact = new contact (phoneNumber, location)
