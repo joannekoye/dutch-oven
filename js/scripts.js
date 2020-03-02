350e979bd5269f63901f6af5e463064a02b54113
Joan Nekoye <joannekoye@gmail.com>
2020-03-02 12:10:55 +0300

    edit ordering options

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,61 @@
+//Business logic
 
-$(document).ready(function(event) {
-  $('#pickUp').click(function() {
-    $("#pickUpOption").show();
-    $('#displayLocation').hide();
-    $('#displayNumber').hide();
-  })
-  $('#deliver').click(function() {
-    $('#displayLocation').show();
-    $('#displayNumber').show();
-    $("#pickUpOption").hide();
-  })
-  function pizza (size, crust, top1, top2) {
+  function pizza (type, size, crust, top1, top2, numberOfPizzas) {
+    this.type = type;
     this.size = size;
     this.crust = crust;
     this.top1= top1;
     this.top2= top2;
+    this.numberOfPizzas = numberOfPizzas;
   }
   
   pizza.prototype.fullOrder= function() {
-    return "You ordered a " + this.size + " " +this.crust +" pizza with " + this.top1 + " and " + this.top2;
+    return "You ordered " +this.numberOfPizzas+" "+ this.size + " " +this.crust +" "+this.type+ " with " + this.top1 + " and " + this.top2 ;
   }
   
   //user-interface
   $(document).ready(function() {
-    $("button#another").click(function() {
-      $("div#size").append( '<select class="form-control" id="size">'+
-                        '<option>Large</option>'+
-                        '<option>Medium</option>'+
-                        '<option>Small</option>'+
-                        '</select>'
-                            );
-          
-      $(".crust").append( '<select class="form-control" id="crust">'+
-                    '<option>Thin</option>'+
-                    '<option>Deep</option>'+
-                    '<option>Cheese-stuffed</option>'+
-                    '</select>'
-                    );
-      $(".top").append('<h4>toppings 1</h4>'+
-                      '<select class="form-control" id="top1">'+
-                      '<option>beef stripes(sh.200)</option>'+
-                      '<option>chicken cubes(sh.200)</option>'+
-                      '<option>pepperoni(sh.200)</option>'+
-                      '</select>'+
-                      '<h4>toppings2</h4>'+
-                      '<select class="form-control" id="top2">'+
-                      '<option>cheese layer(sh.200)</option>'+
-                      '<option>pineapples(sh.200)</option>'+
-                     ' <option>sweet corn(sh.200)</option>'+
-                    '</select>')
-      });
-    $("button#deliver").click(function(event) {
+    $('#pickUp').click(function(event) {
       event.preventDefault();
-      var address=prompt("Enter your Address :");
-      alert("Your order has been received and will be delivered to you in " + address)    
+      $("#pickUpOption").show();
+      $('#deliverOption').hide();
+
+      var type =$('#type').val(); 
       var size=$("#size").val();
       var crust=$("#crust").val();
       var top1=$("#top1").val();
       var top2=$("#top2").val();
-      var newOrder= new pizza (size , crust, top1, top2)
+      var numberOfPizzas = $('#numberOfPizzas').val();
+
+      var newOrder= new pizza (type, size , crust, top1, top2, numberOfPizzas)
       
-      $("ul#customer-order").append("<li><span class=order>" + newOrder.fullOrder() + "Delivered(sh.100)" + "</span></li>");
+      $('#deliveryOrderDetails').hide();
+      $("ul#pickUpOrderDetails").show();
+      $("ul#pickUpOrderDetails").append("<li><span class=order>" + newOrder.fullOrder() + "</span></li>");
+    })
+    
+    $("button#deliver").click(function(event) {
+      event.preventDefault();
+      $("#pickUpOption").hide();
+      $('#deliverOption').show();  
+
+      var address=$('#location').val();
+      $('#deliverOption').text("YOUR ORDER WILL BE DELIVERED TO YOU AT: " + address)
+
+      var type =$('#type').val(); 
+      var size=$("#size").val();
+      var crust=$("#crust").val();
+      var top1=$("#top1").val();
+      var top2=$("#top2").val();
+      var numberOfPizzas = $('#numberOfPizzas').val();
+
+      var newOrder= new pizza (type, size , crust, top1, top2, numberOfPizzas)
+      
+      $('#pickUpOrderDetails').hide();
+      $('ul#deliveryOrderDetails').show();
+      $("ul#deliveryOrderDetails").append("<li><span class=order>" + newOrder.fullOrder() + " <br> (Delivery Charge: $ 1)" + "</span></li>");
     });
-      $("button#make-order").click(function(event) {
-        event.preventDefault();
-        var size=$("#size").val();
-        var crust=$("#crust").val();
-        var top1=$("#top1").val();
-        var top2=$("#top2").val();
-        var newOrder= new pizza (size , crust, top1, top2)
-        
-        $("ul#customer-order").append("<li><span class=order>" + newOrder.fullOrder() + "</span></li>"); 
-      })
+       
+  
   })
     
-})
