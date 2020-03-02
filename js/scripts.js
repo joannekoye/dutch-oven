5744c0243a051dbec7b1c7bc015d1cfe00abb42a
Joan Nekoye <joannekoye@gmail.com>
2020-03-02 15:33:20 +0300

    calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,12 @@
     return parseInt(numberOfPizzas);
   }
 
-  function totalPrice (event) {
-    event.preventDefault();
+  function calculatePrice (e) {
+    var totalPrice = (sizeCost()+crustCost()+top1Cost()+top2Cost()*numberOfPizzasCost());
+    console.log (totalPrice);
     
+    $('#totalPrice').text( + totalPrice + "  Time: 30 min.")
   }
-
 
   function pizza (type, size, crust, top1, top2, numberOfPizzas) {
     this.type = type;
