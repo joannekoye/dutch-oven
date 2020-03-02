405acb1b6232fcc9d67e55a851288b616d0445ce
Joan Nekoye <joannekoye@gmail.com>
2020-03-02 05:24:17 +0300

    write Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,59 @@
+var totalPriceArray = [];
+function order (pizzaSize, crust) {
+  this.type = 5;
+  this.size= pizzaSize;
+  this.crust = crust;
+  this.toppings = 3;
+  this.pizzaPrice = 0;
+}
+order.prototype.pizzaCost = function () {
+  if(this.pizzaSize === "Small(serves 1)") {
+    this.pizzaPrice += 3;
+  }else if(this.pizzaSize ==="Medium(serves 2)") {
+    this.pizzaPrice +=6;
+  }else if(this.pizzaSize==="Large(serves 3-4)") {
+    this.pizzaPrice +=9;
+  }else if(this.pizzaSize==="Mega Large(serves 6)") {
+    this.pizzaPrice +=9;
+  }
+
+  if (this.crust === "") {
+    this.pizzaPrice += 0;
+  }else if(this.crust==="") {
+    this.pizzaPrice +=1;
+  }else if(this.crust==="") {
+    this.pizzaPrice +=2;
+  }
+
+  this.pizzaPrice += this.toppings;
+  return this.pizzaPrice;
+}
+order.prototype.finalCost = function () {
+  var totalCartPrice = 0;
+  for (var arrayElement = 0; arrayElement<totalPriceArray.length;arrayElement++){
+    totalCartPrice += totalPriceArray[arrayElement];
+  }
+  return totalCartPrice;
+}
+
+function Address (phoneNumber, location) {
+  this.phoneNumber = phoneNumber;
+  this.location = location;
+  this.deliveryAddress = (phoneNumber + "  " + location);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(event) {
   $('#pickUp').click(function() {
     $("#pickUpOption").show();
@@ -13,6 +69,10 @@ $(document).ready(function(event) {
     event.preventDefault();
     var location = $('input#location').val();
     var phoneNumber = $('input#number').val();
+    var newContact = new contact (phoneNumber, location)
+    $('#deliverOption').show();
+    $('#contactAddress').text( + newContact.deliveryAddress);
+
     var type = $('select#pizzaType').val();
     var size = $('select#pizzaSize').val();
     var crust = $('select#crust').val();
@@ -20,7 +80,12 @@ $(document).ready(function(event) {
     var toppings = [];
     $.each($("input[name='toppings']:checked"), function(){
       toppings.push($(this).val());
-  });
-  alert("My favourite toppings are: " + toppings.join(", "));
+    });
+    var allTopings = toppings.join(", ");
+    var pizzaDetails=(size + " " + type+", " +crust+" crust. Toppings: " +allTopings);
+    var newOrder = new order (pizzaSize,crust);
+    newOrder.pizzaCost();
+    totalPriceArray.push(newOrder.pizzaPrice);
+    $('#orderDetails').append(+ pizzaDetails);
   })
 })
